ff131b8c8af39a3c164f48d0552ea99251587999
artyomakulov <artyomakulov@gmail.com>
2022-01-21 01:02:14 +0200

    final

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
-
+// import SimpleLightbox from "../node_modules/simplelightbox/dist/simple-lightbox.min.css"
 
 const refs = {
     form: document.querySelector('.search-form'),
@@ -13,12 +13,13 @@ const refs = {
   loadMoreBtn: document.querySelector('.load-more'),
   
 }
+let gallerySet = new SimpleLightbox('.gallery a');
 
 let counter = 1;
 
 const KEY = '25323007-3d609b483f4fcb74c4bf2a361';
 const BASIC_URL = 'https://pixabay.com/api/';
-const SEARCH = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=5';
+const SEARCH = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
 
 
 refs.form.addEventListener('submit', renderEvent);
@@ -27,6 +28,7 @@ refs.loadMoreBtn.addEventListener('click', loadMoreEvent);
 async function renderEvent(event) {
   event.preventDefault()
   refs.div.innerHTML = '';
+   counter = 1
   await axios.get(`${BASIC_URL}?key=${KEY}&q=${refs.input.value}&${SEARCH}&page=${counter}`)
     .then(data => {
       if (data.data.hits.length === 0) {
@@ -34,10 +36,10 @@ async function renderEvent(event) {
         return;
       }
       Notiflix.Notify.success(`Hooray! We found ${data.data.totalHits} images.`)
+     
       createRender(data.data.hits)
-      btnDisable()
+      btnEnable()
     })
-  
 }
 
 function createRender(gallery) {
@@ -45,26 +47,30 @@ function createRender(gallery) {
   {
     return `<div class="photo-card">
     <a href="${largeImageURL}">
-  <img width=400 height=400 src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <img width=400 height=300 src="${webformatURL}" alt="${tags}" loading="lazy" />
   </a>
   <div class="info">
     <p class="info-item">
-      <b>Likes${likes}</b>
+      <b>Likes<br>
+      ${likes}</b>
     </p>
     <p class="info-item">
-      <b>Views${views}</b>
+      <b>Views<br>
+      ${views}</b>
     </p>
     <p class="info-item">
-      <b>Comments${comments}</b>
+      <b>Comments<br>
+      ${comments}</b>
     </p>
     <p class="info-item">
-      <b>Downloads${downloads}</b>
+      <b>Downloads<br>
+      ${downloads}</b>
     </p>
   </div>
 </div>`
-  }
-  )
+  })
   refs.div.insertAdjacentHTML('beforeend', createGallery.join(''))
+  gallerySet.refresh()
 }
 
 async function loadMoreEvent() {
@@ -73,19 +79,20 @@ async function loadMoreEvent() {
         .then(data => {
       if (data.data.hits.length === 0) {
         Notiflix.Notify.warning(`We're sorry, but you've reached the end of search results.`);
-        return;
+        return btnDisable()
       }
-      Notiflix.Notify.success(`Hooray! We found ${data.data.totalHits} images.`)
-    createRender(data.data.hits)
+          Notiflix.Notify.success(`Hooray! We found ${data.data.totalHits} images.`)
+            setTimeout(() => {
+            createRender(data.data.hits)
+          }, 500);
     })
 }
 
 function btnDisable() {
-  refs.loadMoreBtn.classList.remove('hiddenBtn')
   refs.loadMoreBtn.disabled = true;
 }
 
-
 function btnEnable() {
+  refs.loadMoreBtn.classList.remove('hiddenBtn')
   refs.loadMoreBtn.disabled = false;
 }
